c19d240e6c14ac938ab7b16f8606d8c45b8ead6e
debratokumar <dkghosh.nbr@gmail.com>
2023-09-21 15:29:39 +0600

    deb

---
+++ sum.js
@@ -0,0 +1,35 @@
+
+/*
+/*let a=5,b=8;
+let c=a+b;
+console.log("sum:"+c);
+*/
+
+//var
+/*const pi=3.14
+let new_name = 'Arnab';
+console.log(pi);
+console.log("Name: " + new_name);
+*/
+/*let array = ['apple', 'banana','grape', 'mango','lichi'];
+console.log(array);
+
+console.log(array.length);
+for (let=i=0; i<array.length; i++)
+
+{
+    console.log(array[i]);
+
+}
+
+{
+
+}
+ */
+
+//json object
+let obj = {
+    "name": 'arnab', age: 18, "designation": 'ap'
+}
+console.log(obj);
+console.log (obj.designation);
